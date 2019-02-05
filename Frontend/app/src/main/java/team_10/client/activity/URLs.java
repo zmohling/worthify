679ec615628ccc714a37da9687bdeccc02e2c648
Kyle Marek <kjmarek@iastate.edu>
2019-02-05 01:14:24 -0600

    correct ip and httptext

--- Frontend/app/src/main/java/team_10/client/activity/URLs.java
+++ Frontend/app/src/main/java/team_10/client/activity/URLs.java
@@ -1,7 +1,7 @@
 package team_10.client.activity;
 
 public class URLs {
-    private static final String ROOT_URL = "http://192.168.101.1/Android/Api.php?apicall=";
+    private static final String ROOT_URL = "http://10.20.20.137/test/Api.php?apicall=";
 
     public static final String URL_REGISTER = ROOT_URL + "signup";
     public static final String URL_LOGIN= ROOT_URL + "login";
