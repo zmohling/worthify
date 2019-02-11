2f077f818909c2ab29471c1ef3956b4dc35ee6bc
gdstout27 <gdstout27@gmail.com>
2019-02-11 13:31:26 -0600

    complying with kyle's json requests

--- Backend/serverApp/src/main/java/com/serverApp/serverApp/controllers/UserController.java
+++ Backend/serverApp/src/main/java/com/serverApp/serverApp/controllers/UserController.java
@@ -1,15 +1,32 @@
 package com.serverApp.serverApp.controllers;
 
 import com.serverApp.serverApp.models.User;
+import com.serverApp.serverApp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController{
+
+    @Autowired
+    UserRepository userRepo;
+
     @RequestMapping("/login")
     public String login(@RequestBody User user){
-        System.out.println("Login: " + user.getFirstName() + " " + user.getLastName());
-        return "success";
+    // System.out.println("Login: Id: " + user.getId() + ": " + user.getFirstName() + " " +
+    // user.getLastName());userRepo.save(user);
+
+        //userRepo.save(user);
+        String rString =
+        "\"error\":\"false\","
+            + "\"message\":\"user login success\","
+            +  "\"user\":{"
+            + "\"id\":\"" + user.getId() + "\"," +
+                "\"username\":\"" +  user.getUserName() + "\"," +
+                "\"email\":\"" + user.getEmail() + "\"}";
+
+        return rString;
     }
 }
--- Backend/serverApp/src/main/java/com/serverApp/serverApp/models/User.java
+++ Backend/serverApp/src/main/java/com/serverApp/serverApp/models/User.java
@@ -12,11 +12,23 @@ public class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "lastName")
+    /*@Column(name = "lastName")
     private String lastName;
 
     @Column(name = "firstName")
-    private String firstName;
+    private String firstName;*/
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Column(name = "userName")
+
+    private String userName;
 
     @Column(name = "email")
     private String email;
@@ -24,8 +36,8 @@ public class User implements Serializable {
     @Column(name = "password")
     private String password;
 
-    @Column(name = "type")
-    private int type;
+    /*@Column(name = "type")
+    private int type;*/
 
     public static long getSerialVersionUID() {
         return serialVersionUID;
@@ -39,21 +51,7 @@ public class User implements Serializable {
         this.id = id;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
 
     public String getEmail() {
         return email;
@@ -69,13 +67,5 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public int getType() {
-        return type;
-    }
-
-    public void setType(int type) {
-        this.type = type;
     }
 }
