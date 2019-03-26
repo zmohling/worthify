597e4d0bd99973012e8857273dc1163d571736ba
gdstout27 <gdstout27@gmail.com>
2019-03-26 17:42:26 -0500

    web implementation almost finished. no working guts.

--- Frontend/admin/client/js/home_js.js
+++ Frontend/admin/client/js/home_js.js
@@ -1,3 +1,22 @@
 window.onload = function(){
-    alert("Admin home page.");
+    //load the users table by default
+    var tabs = document.getElementById("Users");
+    tabs.style.display = "block";
+    
+
+}
+
+function switchTable(evt, tableName) {
+    //switch the table that is being displayed
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tableName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
