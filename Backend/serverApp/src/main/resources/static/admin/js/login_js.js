3b5485917e2f2deeeac18ba92e7a7b39a3bf96a5
gdstout27 <gdstout27@gmail.com>
2019-04-07 15:29:12 -0500

    updating login

--- Backend/serverApp/src/main/resources/static/admin/js/login_js.js
+++ Backend/serverApp/src/main/resources/static/admin/js/login_js.js
@@ -1,3 +1,23 @@
 function login(){
-    
+    var url = "http://cs309-jr-1.misc.iastate.edu:8080/login";
+    //url = "http://localhost:8080/login";//for testing purposes
+
+    var userInfo = {
+        email:document.getElementById("email").value,
+        password:document.getElementById("pass").value
+    };
+
+    $.ajax({contentType: 'application/json', dataType: 'json', type:'POST', url: url, data:JSON.stringify(userInfo),
+    success: function(json){
+        console.log(json);
+        if(json.user.type == 1 && json.message == "user login success"){
+            console.log(json)
+            console.log(json.user.authorization);
+            //window.location.href= "/admin/home.html?auth=" + json.user.authorization;
+        }
+    },
+    error: function(){
+        //do nothing
+    }
+    });
 }
