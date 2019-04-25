885a4eefcc7be841f677dbd6de7b08c7571d2d09
Michael Davis <michaeld@iastate.edu>
2019-04-25 16:40:08 -0500

    implemented an request to get the stock history of an account going back 5 years or back until the user reportedly invested, which ever is later

--- Backend/serverApp/src/main/java/com/serverApp/serverApp/other/StockRetrieval.java
+++ Backend/serverApp/src/main/java/com/serverApp/serverApp/other/StockRetrieval.java
@@ -3,6 +3,7 @@ package com.serverApp.serverApp.other;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.hibernate.type.DateType;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -10,6 +11,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * class to handle the stock apis and return them
@@ -36,10 +41,10 @@ public class StockRetrieval {
         return content.toString();
     }
 
-    public String retrieve2yData(String ticker) throws java.io.IOException {
+    public String retrieve2yData(String ticker, Date date) throws java.io.IOException {
         URL accountURL;
         String url = "https://api.iextrading.com/1.0/stock/"
-                + ticker + "/chart/2y";
+                + ticker + "/chart/5y";
         accountURL = new URL(url);
         HttpURLConnection con = (HttpURLConnection) accountURL.openConnection();
         con.setRequestMethod("GET");
@@ -54,11 +59,16 @@ public class StockRetrieval {
         JsonParser jsonParser = new JsonParser();
         JsonArray arr = jsonParser.parse(content.toString()).getAsJsonArray();
         String returnStr = "[ {";
+        boolean first = true;
         for(int i = 0; i < arr.size(); i++) {
-            if(i != 0) {
-                returnStr = returnStr + ";";
+            Date iterDate = java.sql.Date.valueOf(LocalDate.parse(arr.get(i).getAsJsonObject().get("date").getAsString()));
+            if(date.compareTo(iterDate) <= 0) {
+                if (!first) {
+                    returnStr = returnStr + ",";
+                }
+                returnStr = returnStr + "\"" + iterDate.toString() + "\"" + ": " + "\"" + arr.get(i).getAsJsonObject().get("high") + "\"";
+                first = false;
             }
-            returnStr = returnStr + arr.get(i).getAsJsonObject().get("date") + ": " + arr.get(i).getAsJsonObject().get("high");
         }
         returnStr = returnStr + "} ]";
         return returnStr;
